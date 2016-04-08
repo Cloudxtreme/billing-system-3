61bd057a85797132b90c258470546211da258207
vbilenko78 <vbilenko@yahoo.com>
2016-04-07 22:10:26 -0700

    added validations to phone and name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ActiveRecord::Base
   has_many :customer_addresses
   has_many :addresses, through: :customer_addresses
   has_many :invoices
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :phone, presence: true,  length: { maximum: 12 }, uniqueness: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 if Rails.env.development?
-  Admin.create email: "vbilenko@yahoo.com", password: "vbilenko@yahoo.com", password_confirmation: "vbilenko@yahoo.com", name: "Vladimir", phone: "503-544-5745"
 
-c =  Customer.create email: "customer1@example.com", name: "Sebastian", phone: "503-786-5867"
+  Admin.create email: "vbilenko@yahoo.com",
+               password: "vbilenko@yahoo.com",
+               password_confirmation: "vbilenko@yahoo.com",
+               name: "Vladimir", phone: "503-544-5745"
 
-c.addresses.create(street_1: "2324 SE Alder Street", street_2: "Apt#14", city: "Portland", state: "OR", postal_code: "97456")
+c =  Customer.create email: "customer1@example.com",
+                     name: "Sebastian",
+                     phone: "503-786-5867"
 
-c.invoices.create(amount: 235.67, description: "Ivoice for my work")
+c.addresses.create(street_1: "2324 SE Alder Street",
+                   street_2: "Apt#14",
+                   city: "Portland",
+                   state: "OR",
+                   postal_code: "97456")
 
+c.invoices.create(amount: 235.67,
+                  description: "Ivoice for my work")
 
 
 end
