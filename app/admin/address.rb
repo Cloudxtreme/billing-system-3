cb241daad7a1e66bac440e587ca4de781c7c5f21
vbilenko78 <vbilenko@yahoo.com>
2016-04-19 20:47:14 -0700

    added strong_parameters to active_admin

--- app/admin/address.rb
+++ app/admin/address.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Address do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+ permit_params :street1, :street2, :city, :state, :label
 #
 # or
 #
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 if Rails.env.development?
 
-  Admin.create email: "vbilenko@yahoo.com",
-    password: "vbilenko@yahoo.com",
-    password_confirmation: "vbilenko@yahoo.com",
+  Admin.create email: "admin-1@example.com",
+    password: "admin-1@example.com",
+    password_confirmation: "admin-1@example.com",
     name: "Vladimir", phone: "503-544-5745"
 
-  c =  Customer.create email: "customer1@example.com",
+  c =  Customer.create email: "customer-1@example.com",
     name: "Sebastian",
     phone: "503-786-5867"
 
@@ -17,5 +17,19 @@ if Rails.env.development?
 
   c.invoices.create(amount: 235.67,
                     description: "Ivoice for my work")
+
+  b =  Customer.create email: "customer-2@example.com",
+    name: "Barbaros",
+    phone: "503-786-7222"
+
+  b.addresses.create(street_1: "1111 SE Alder Street",
+                     street_2: "Apt#345",
+                     city: "Portland",
+                     state: "OR",
+                     postal_code: "97644")
+
+  b.invoices.create(amount: 235.67,
+                    description: "Ivoice for profit!!!")
+
 
 end
